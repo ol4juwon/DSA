a0768d4ef0ab08b2af9b7752bd365914ae11cec1
olajuwon <olajuwonlawal2012@gmail.com>
2025-06-25 00:39:57 +0100

    Bubble sort

--- src/main/java/Search/SearchAlgorithms.java
+++ src/main/java/Search/SearchAlgorithms.java
@@ -1,3 +1,5 @@
+package Search;
+
 import java.time.Instant;
 import java.util.Arrays;
 
---
+++ src/main/java/Sorting/BubbleSort.java
@@ -0,0 +1,32 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] nums = {3,5,1,2,10,6,21,34,15};
+        System.out.println("Before sorting"+Arrays.toString(nums) );
+        sort(nums);
+
+            System.out.println("After sorting"+Arrays.toString(nums) );
+
+    }
+
+    public static void sort(int[] nums) {
+        int size = nums.length;
+        int steps =0;
+        // iterate over  values
+        for (int i = 0; i < size; i++) {
+            for (int j = 1; j < (size - i); j++) {
+                steps++;
+                if (nums[j - 1] > nums[j]) {
+                    // swap values
+                    int temp = nums[j - 1];
+                    nums[j - 1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        System.out.println("steps: "+steps);
+    }
+}
